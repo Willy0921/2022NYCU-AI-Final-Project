2995fceceb246f3721e461b8f9796fe21c6cf362
Benson Parker <99243360+Benson5376@users.noreply.github.com>
2022-06-10 20:34:57 +0800

    Add files via upload

---
+++ atari.py
@@ -0,0 +1,43 @@
+import gym
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from settings import *
+
+
+frame_proc = T.Compose([T.ToPILImage(),
+                        T.Grayscale(), \
+                        T.Resize((84,84), interpolation=Image.BILINEAR), \
+                        T.ToTensor()])
+
+class Atari(object):
+    def __init__(self, env_name="Freeway-v0", agent_history_length=4, render=False):
+        if render:
+            self.env = gym.make(env_name, render_mode="human")
+        else:
+            self.env = gym.make(env_name)
+        self.state = None
+        self.agent_history_length = agent_history_length
+
+    def reset(self):
+        observation = self.env.reset()
+        frame = self.image_proc(observation).to(DEVICE)
+        self.state = frame.repeat(1,self.agent_history_length,1,1)
+        return self.state
+
+    def image_proc(self, image):
+        return frame_proc(image)
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        frame = self.image_proc(observation).to(DEVICE)
+        next_state = torch.cat( (self.state[:, 1:, :, :], frame.unsqueeze(0)), axis=1 )
+        self.state = next_state
+        return next_state, reward, done, info
+
+    def get_render(self):
+        observation = self.env.render(mode='rgb_array')
+        return observation
+
+    def render(self):
+        self.env.render()
